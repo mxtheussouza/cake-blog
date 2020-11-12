ec5621fd8d0410dfc99c658ca936bbc4de247dce
Matheus Araújo <kaienmatheus@gmail.com>
2020-11-12 13:01:13 -0300

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:5-apache
+FROM php:5.6-apache
 
 WORKDIR /var/www/html
 
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Run mysql container
-docker run --name docker-mysql -v /home/matheus/Documentos/projects/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=k@ien123 --restart always -d mysql
+docker run --name docker-mysql -v /home/go/Documents/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=k@ien123 --restart always -d mysql:5.7
 
 #Run phpmyadmin container
 docker run --name docker-phpmyadmin -d --link docker-mysql:db -p 8081:80 -d phpmyadmin/phpmyadmin 
