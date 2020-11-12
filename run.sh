f0b096f83662f63d34c77b94726b32bfbe0c72ed
Matheus Araújo <61164981+matheusdearaujo@users.noreply.github.com>
2020-11-12 16:40:28 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 mkdir /home/$USER/mysql
 
 #Run MySQL container
-docker run --name docker-mysql -v /home/$USER/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=k@ien123 --restart always -d mysql:5.7
+docker run --name docker-mysql -v /home/$USER/mysql/:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 --restart always -d mysql:5.7
 
 #Run phpmyadmin container
 docker run --name docker-phpmyadmin -d --link docker-mysql:db -p 8180:80 -d phpmyadmin/phpmyadmin 
